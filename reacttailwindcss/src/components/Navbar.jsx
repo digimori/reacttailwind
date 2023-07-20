dde3c2749d32557789a98021dd4ae85acebf070d
digimori <hanaorikuu@gmail.com>
2023-07-20 14:10:55 +0000

    feat: Navbar implemented with breakpoint logic and useState toggle

--- reacttailwindcss/src/components/Navbar.jsx
+++ reacttailwindcss/src/components/Navbar.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
+import { useSyncExternalStore } from 'react';
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img src={logo} alt="logo" className='w-[124px] height-[32px]' />
+      {/* Desktop Screens navs mapped */}
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+        {navLinks.map((nav, index) => (
+          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length -1 ? 'mr-0' : 'mr-10'}`}> {/* This is a ternary if statement to stop there being too much margin on the final link */}
+            <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+        ))}
+      </ul>
+          <div className='sm:hidden flex flex-1 justify-end items-center'>
+            {/* close/menu are referencing the SVG files, so "If close is showing then on click show menu and vice versa" the toggle is the useState referenced above and below */}
+            <img src={toggle ? close : menu} 
+            alt="menu"
+            className='w-[28px] h-[28px] object-contain'
+            onClick={() => setToggle((prev) => !prev)} /> {/* Re-assignment using useState. Object is immutable so must reference the previous state as prev/!prev instead */}
+            {/* The below: If toggle is switched on, show flex container, else, stay hidden */}
+            <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}></div>
+          </div>
+    </nav>
   )
 }
 
