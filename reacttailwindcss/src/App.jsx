834e29621377ddcc561d4ca3482725226a41c30b
digimori <hanaorikuu@gmail.com>
2023-07-20 12:52:18 +0000

    Tailwind CSS styles put into own file and imported to App.jsx

--- reacttailwindcss/src/App.jsx
+++ reacttailwindcss/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import styles from './style';
 
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='bg-primary w-ful overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          navbar here
+        </div>
+      </div>
+
+      
+    </div>
   )
 }
 
