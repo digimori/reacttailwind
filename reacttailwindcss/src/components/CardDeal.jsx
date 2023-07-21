696184f3b5c23ed725ed9af55559eacb37e28910
digimori <hanaorikuu@gmail.com>
2023-07-21 11:47:35 +0000

    feat: CardDeal.jsx copied from Business component - Needs additional styling for background.

--- reacttailwindcss/src/components/CardDeal.jsx
+++ reacttailwindcss/src/components/CardDeal.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import styles from '../style';
+import styles, { layout } from '../style';
 import { card } from '../assets';
 
 const CardDeal = () => {
   return (
-    <section className=''>
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal<br className='sm:block hidden'/> in a few easy steps</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor.
+        ç Aliquet ultrices ac, ametau.</p>
 
+          <Button styles="mt-10"/>
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        <img src={card} alt="carddeal" />
+      </div>
     </section>
   )
 }
